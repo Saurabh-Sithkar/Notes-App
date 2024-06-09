14d66882a88aa8eb6c63820991e27f6d0ff3b5c4
Saurabh-Sithkar <pacific.learning0103@gmail.com>
2024-06-09 14:34:46 +0530

    Done!!!

---
+++ src/pages/Notes.jsx
@@ -0,0 +1,51 @@
+import {CiSearch} from 'react-icons/ci'
+// import dummyNotes from '../dummy_notes'
+import { Link } from 'react-router-dom'
+import {BsPlusLg} from 'react-icons/bs'
+import {MdClose} from 'react-icons/md'
+
+
+import NoteItem from '../components/NoteItem'
+import { useEffect, useState } from 'react'
+
+
+const Notes = ({notes}) => {
+    const [showSearch,setShowSearch] = useState(false);
+    const [text,setText] = useState('')
+    const [filteredNotes, setFilteredNotes] = useState(notes)
+
+    const handleSearch = () => {
+        setFilteredNotes(notes.filter(note =>{
+            if(note.title.toLowerCase().match(text.toLocaleLowerCase())){
+                return note;
+            }
+        }))
+    }
+
+    useEffect(handleSearch,[text])
+
+  return (
+   
+        <section>
+            <header className='notes__header'>
+               {!showSearch && <h2>My Notes</h2>} 
+                {showSearch && <input type="text" value={text} onChange={(e) =>{setText(e.target.value); handleSearch();}} autoFocus placeholder='Keyword...' />}
+
+                <button className='btn' onClick={() => setShowSearch(prevState => !prevState)}>
+                    {showSearch? <MdClose/>:<CiSearch/>}</button>
+            </header>
+
+            <div className="notes__container">
+                {filteredNotes.length == 0 && <p className='empty__notes'>No notes found</p>}
+                {
+                    filteredNotes.map(note => <NoteItem key={note.id} note={note}/>)
+                }
+            </div>
+
+            <Link to="/create-note" className='btn add__btn'><BsPlusLg/></Link>
+        </section>
+
+  )
+}
+
+export default Notes
